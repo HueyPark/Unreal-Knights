61b3bf5f1cb70f94840be52baa884a5a136889cd
HueyPark <jaewan.huey.park@gmail.com>
2016-08-27 01:03:49 +0900

    Add: UserUI PowerProgressBar

--- Client/Code/Source/UnrealKnights/User/UserUI.cpp
+++ Client/Code/Source/UnrealKnights/User/UserUI.cpp
@@ -2,8 +2,45 @@
 
 #include "UnrealKnights.h"
 #include "UserUI.h"
+#include "Blueprint/WidgetTree.h"
+#include "Slate/SlateBrushAsset.h"
+#include "Components/ProgressBar.h"
+
+float MIN_POWER = 0.0f;
+float MAX_POWER = 1.0f;
+
+void UUserUI::NativeConstruct()
+{
+	PowerState = EPowerState::Up;
+	Power = 0.0f;
+	PowerProgressBar = WidgetTree->FindWidget<UProgressBar>("PowerProgressBar");
+}
+
+void UUserUI::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	if (PowerState== EPowerState::Up)
+	{
+		Power += InDeltaTime;
+		if (Power > MAX_POWER)
+		{
+			Power = MAX_POWER;
+			PowerState = EPowerState::Down;
+		}
+	}
+	else
+	{
+		Power -= InDeltaTime;
+		if (Power < MIN_POWER)
+		{
+			Power = MIN_POWER;
+			PowerState = EPowerState::Up;
+		}
+	}
+
+	PowerProgressBar->SetPercent(Power);
+}
 
 void UUserUI::OnHitButton()
 {
-	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Hit!"));
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Hit: %f"), Power));
 }
--- Client/Code/Source/UnrealKnights/User/UserUI.h
+++ Client/Code/Source/UnrealKnights/User/UserUI.h
@@ -5,10 +5,29 @@
 #include "Blueprint/UserWidget.h"
 #include "UserUI.generated.h"
 
+class UProgressBar;
+
 UCLASS()
 class UNREALKNIGHTS_API UUserUI : public UUserWidget
 {
 	GENERATED_BODY()
+
+private:
+	enum class EPowerState
+	{
+		Up,
+		Down
+	};
+
+private:
+	EPowerState PowerState;
+	float Power;
+	UProgressBar* PowerProgressBar;
+
+public:
+	virtual void NativeConstruct();
+
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime);
 
 protected:
 	UFUNCTION(BlueprintCallable, Category = "UserUI")

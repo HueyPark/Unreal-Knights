83cd701ffbadd5640df9dce9d55801894f7157ae
HueyPark <jaewan.huey.park@gmail.com>
2016-08-27 21:59:00 +0900

    Add: Lobby Level, LobbyGameMode

---
+++ Client/Code/Source/UnrealKnights/Level/Lobby/LobbyGameMode.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "LobbyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALKNIGHTS_API ALobbyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+};
